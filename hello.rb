4a80104dbc045bc4ca70d14793622a09387fb188
matteomanzo <matteo.manzo@comparethemarket.com>
2016-01-12 17:44:56 +0000

    deleted unnecessary files

--- hello.rb
+++
@@ -1,5 +0,0 @@
-# Default is World
-# Author: Matteo Manzo (imatteo.manzo@gmail.com)
-name = ARGV.first || "World"
-
-puts "Hello, #{name}!"
--- lib/hello.rb
+++
@@ -1,7 +0,0 @@
-require 'greeter'
-# Default is World
-# Author: Matteo Manzo (imatteo.manzo@gmail.com)
-name = ARGV.first || "World"
-
-greeter = Greeter.new(name)
-puts greeter.greet
